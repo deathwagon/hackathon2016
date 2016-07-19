cc4d535e64a67bbf90d96d2e7722ec33593a0059
Stuart Branham <glompix@gmail.com>
2016-07-19 11:21:04 -0700

    Scaffold GetActivePages

---
+++ src/api-seo/Models/ActivePage.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_seo.Models
+{
+    public class ActivePage
+    {
+        public string Name { get; set; }
+        public string EnPath { get; set; }
+    }
+}
