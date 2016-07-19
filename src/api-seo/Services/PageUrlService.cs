a44730b25f12e31ff413c2430fa13fa3b9269ec8
Rajendar Vontela <rvontela@godaddy.com>
2016-07-19 14:13:42 -0700

    added pageurlservice

---
+++ src/api-seo/Services/PageUrlService.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PageUrlService : IPageUrlService
+{
+    public string GetMarket(string pageUrl)
+    {
+        var result = string.Empty;
+        
+        Uri pageUri;
+        if (Uri.TryCreate(pageUrl, UriKind.Absolute, out pageUri))
+        {
+            int iqs = pageUrl.IndexOf('?');
+            if (iqs > 0)
+            {
+                Dictionary<string, string> dicQueryString = 
+                        pageUrl.Substring(iqs+1).Split('&')
+                            .ToDictionary(c => c.Split('=')[0],
+                                        c => Uri.UnescapeDataString(c.Split('=')[1]));
+
+                result = dicQueryString["market"];     
+            }
+        }
+
+        return !string.IsNullOrEmpty(result) ? result : "en";
+    }
+}
