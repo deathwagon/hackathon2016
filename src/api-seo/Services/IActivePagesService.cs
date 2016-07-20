7ec5491ddd4a91eb47b42d2354dd546281b8e705
Rajendar Vontela <rvontela@godaddy.com>
2016-07-19 18:25:50 -0700

    more

--- src/api-seo/Services/IActivePagesService.cs
+++ src/api-seo/Services/IActivePagesService.cs
@@ -7,7 +7,6 @@ namespace api_seo.Services
     public interface IActivePagesService
     {
         IEnumerable<PageDataModel> GetAll(string appId, string market);
-        PageData Get(Guid id);
         void Create(PageDataModel model);
         void Update(Guid id, PageDataModel model);
         void Delete(Guid id);
