7dacd004d8f2821ec01144bf1ef9bfe46e71d5b7
Stuart Branham <glompix@gmail.com>
2016-07-19 18:18:35 -0700

    REST API for page management

--- src/api-seo/Startup.cs
+++ src/api-seo/Startup.cs
@@ -35,6 +35,7 @@ namespace api_seo
             services.AddTransient<IRedirectService, RedirectService>();
             services.AddTransient<IPageUrlService, PageUrlService>();
             services.AddSingleton<ICassandraData, CassandraData>();
+            services.AddSingleton<IPageService, PageService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
