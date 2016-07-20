8822d56ba282977b95f56f84769c632764677c92
mmicco <mmicco@godaddy.com>
2016-07-19 17:32:57 -0700

    initial pagelookup methods

--- src/api-seo/Services/PageLookupService.cs
+++ src/api-seo/Services/PageLookupService.cs
@@ -4,16 +4,28 @@ namespace api_seo.Services
 {
     public class PageLookupService : IPageLookupService
     {
-        public PageLookupService()
+        private ICassandraData _cassandraData; 
+        public PageLookupService(ICassandraData cassandraData)
         {
-            
+            _cassandraData = cassandraData;
         }
         
         public PageLookupData GetPage(string appId, string market, string path)
         {
-            // #1 Redirect
-            // #2 Not Found
-            // #3 Found, show page data
+            var query = "select * from seo.pages_data_active";
+            var results = _cassandraData.ActiveSession.Execute(query);
+
+            foreach(var rowData in results) 
+            {
+                var result = new PageLookupData
+                {
+                    PageId = rowData.GetValue<string>("page_id"),
+                    Market = rowData.GetValue<string>("market"),
+                    StatusCode = 200
+                };
+
+                return result;
+            }
 
             return null;
         }
